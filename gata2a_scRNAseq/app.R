df20d14a3c4e91ba514d99ae53541a6575338b19
diazdc <walktheline@gmail.com>
2020-02-25 18:02:45 -0600

    git test 2

--- gata2a_scRNAseq/app.R
+++ gata2a_scRNAseq/app.R
@@ -24,7 +24,7 @@ gene_df <- read.table("./data/Danio_Features_unique_Ens98_v1.tsv",
 # gene_names_df <- gene_names_df[,c(1,2,5,3,4)]
 
 # git test 1
-
+# git test 2
 
 gene_df <- gene_df[gene_df$Gene.name.uniq %in% rownames(seurat_obj),]
 ens_id <- gene_df$Gene.stable.ID

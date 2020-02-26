5d4ca4cbc33ebb185163411fcb77f5250dfebf26
diazdc <walktheline@gmail.com>
2020-02-25 18:00:59 -0600

    this is a test

--- gata2a_scRNAseq/app.R
+++ gata2a_scRNAseq/app.R
@@ -23,6 +23,8 @@ gene_df <- read.table("./data/Danio_Features_unique_Ens98_v1.tsv",
 # gene_names_df$in_dataset <- rownames(seurat_obj) %in% gene_names_df$gene_name
 # gene_names_df <- gene_names_df[,c(1,2,5,3,4)]
 
+
+
 gene_df <- gene_df[gene_df$Gene.name.uniq %in% rownames(seurat_obj),]
 ens_id <- gene_df$Gene.stable.ID
 com_name <- gene_df$Gene.name.uniq
@@ -461,4 +463,4 @@ shinyApp(ui = ui, server = server)
 # options(shiny.reactlog=TRUE, shiny.fullstacktrace = TRUE); shiny::runApp("/Volumes/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/gata2a_scRNAseq/app.R")
 
 # start R session
-# rsconnect::deployApp('/Volumes/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/gata2a_scRNAseq, account = 'piotrowskilab')
+# rsconnect::deployApp('/Volumes/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/gata2a_scRNAseq, accoun

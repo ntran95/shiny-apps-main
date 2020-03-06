182d51260d281319ed3a2698a924be372321a7eb
diazdc <walktheline@gmail.com>
2020-03-06 15:29:17 -0600

    added source url too app.R

--- all_regeneration_datasets_Sungmin/app.R
+++ all_regeneration_datasets_Sungmin/app.R
@@ -6,38 +6,46 @@ library(shinythemes)
 library(shinyWidgets)
 library(dplyr)
 
+# Git branch
+branch <- "split-app"
 
 # =================================================================== Preamble
-
+source(paste0("https://github.com/diazdc/shiny-apps-main/",
+  "blob/", branch,"/all_regeneration_datasets_Sungmin/preamble.R"))
 
 # ===================================================================== Server
-
+source(paste0("https://github.com/diazdc/shiny-apps-main/",
+  "blob/", branch,"/all_regeneration_datasets_Sungmin/server.R"))
 
 # ========================================================================= UI
-
+source(paste0("https://github.com/diazdc/shiny-apps-main/",
+  "blob/", branch,"/all_regeneration_datasets_Sungmin/ui.R"))
 
 # =============================================================================
 shinyApp(ui = ui, server = server)
 
 
-# ==== Command line tools
-# Upload app to shinyaqpps.io (must be directory that contains app.R)
-# start R session
-
-# Deploy to shinyapps.io
-# if you have repo issues with bioconductor
-
+# ==== Deploy/execute tools
 # options(repos = BiocManager::repositories())
 # getOption("repos")
 
+if (FALSE) {
+  # Deploy local
+  rsconnect::deployApp(paste0("/Volumes/projects/ddiaz/Analysis/",
+    "Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin"),
+    account = "piotrowskilab")
+  # Server
+  rsconnect::deployApp(paste0("/n/projects/ddiaz/Analysis/",
+    "Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin"),
+    account = "piotrowskilab")
 
-# rsconnect::deployApp('/Volumes/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin', account = 'piotrowskilab')
-# rsconnect::deployApp('/n/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin', account = 'piotrowskilab')
+  #Execute app locally
+  options(shiny.reactlog=TRUE, shiny.fullstacktrace = TRUE)
+  shiny::runApp(paste0("/Volumes/projects/ddiaz/Analysis/",
+    "Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin/app.R"))
 
-# Execute app locally
-# options(shiny.reactlog=TRUE, shiny.fullstacktrace = TRUE); shiny::runApp('/Volumes/projects/ddiaz/Analysis/Scripts/rsconnect/shinyapps.io/all_regeneration_datasets_Sungmin/app.R')
-
-# Logs
-# rsconnect::showLogs(account = 'piotrowskilab', appName = 'all_regeneration_datasets_Sungmin')
-
+  # Logs
+  rsconnect::showLogs(account = 'piotrowskilab',
+    appName = 'all_regeneration_datasets_Sungmin')
+}
 # available.packages(contriburl = "https://cran.rstudio.com")

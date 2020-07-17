e17ed0f2d629b597ffecf4cb7efecf3d0d6cf949
Gioi Tran <ntran2@uoregon.edu>
2020-07-17 15:42:28 -0700

    save work: building indiv. cell hmap df from scratch

--- all_regeneration_datasets_Sungmin/data/process_seurObj.R
+++ all_regeneration_datasets_Sungmin/data/process_seurObj.R
@@ -98,11 +98,126 @@ g <- ggplot(dotplot$data, aes(id, features.plot,fill= avg.exp.scaled, width = 1,
 
 
 
-g <- g + labs(title = paste("Selected analysis:",
-                            as.character(input$Analysis)), subtitle = "", caption = "") +
-  theme(plot.title = element_text(face = "plain", size = 14))
+# =================== Individual Cell Heatmap =====================
+"%||%" <- devtools:::`%||%`
+
+p <- DoHeatmap(seurat_Obj, features = features,slot = "data", group.by = "cell.type.ident.by.data.set") + NoLegend()
+View(p$data)
+p$data$scaled.exp <- scale(p$data$Expression)
+
+#check
+DefaultAssay(seurat_Obj) <- "RNA"
+seurat_Obj <- NormalizeData(seurat_Obj)
+seurat_Obj <- FindVariableFeatures(seurat_Obj, selection.method = "vst", nfeatures = 2000)
+seurat_Obj <- ScaleData(seurat_Obj, features = rownames(seurat_Obj))
+q <- DoHeatmap(seurat_Obj, features = features,slot = "scale.data", group.by = "cell.type.ident.by.data.set") + NoLegend()
+View(p$data)
+
+#de novo scaling calculations
+#from DotPlot()
+group.by <- "cell.type.ident.by.data.set"
+Idents(seurat_Obj) <- "cell.type.ident.by.data.set"
+cells <- unlist(x = CellsByIdentities(object = seurat_Obj))
+data.features <- FetchData(object = seurat_Obj, vars = features, cells = cells)
+data.features$id <- if (is.null(x = group.by)) {
+  Idents(object = object)[cells, drop = TRUE]
+} else {
+  object[[group.by, drop = TRUE]][cells, drop = TRUE]
+}
+if (!is.factor(x = data.features$id)) {
+  data.features$id <- factor(x = data.features$id)
+}
+id.levels <- levels(x = data.features$id)
+data.features$id <- as.vector(x = data.features$id)
+data.plot <- lapply(
+  X = unique(x = data.features$id),
+  FUN = function(ident) {
+    data.use <- data.features[data.features$id == ident, 1:(ncol(x = data.features) - 1), drop = FALSE]
+    avg.exp <- apply(
+      X = data.use,
+      MARGIN = 2,
+      FUN = function(x) {
+        return(mean(x = expm1(x = x)))
+      }
+    )
+      #pct.exp <- apply(X = data.use, MARGIN = 2, FUN = PercentAbove, threshold = 0)
+      return(avg.exp = avg.exp)
+  }
+    )
+names(x = data.plot) <- unique(x = data.features$id)
+
+#from DoHeatMap
+group.by <- "cell.type.ident.by.data.set"
+draw.lines <- TRUE
+data <- as.data.frame(x = t(x = as.matrix(x = GetAssayData(
+  object = seurat_Obj, slot = "data")[features, cells, drop = FALSE])))
+
+data$id <- if (is.null(x = group.by)) {
+  Idents(object = object)[cells, drop = TRUE]
+} else {
+  object[[group.by, drop = TRUE]][cells, drop = TRUE]
+}
+if (!is.factor(x = data$id)) {
+  data$id <- factor(x = data$id)
+}
+id.levels <- levels(x = data$id)
+data$id <- as.vector(x = data$id)
+
+group.by <- group.by %||% 'ident'
+group.use <- object[[group.by]][cells, , drop = FALSE]
+plots <- vector(mode = 'list', length = ncol(x = groups.use))
+for (i in 1:ncol(x = groups.use)) {
+  data.group <- data
+  group.use <- groups.use[, i, drop = TRUE]
+  if (!is.factor(x = group.use)) {
+    group.use <- factor(x = group.use)
+  }
+  names(x = group.use) <- cells
+  # if (draw.lines) {
+  #   # create fake cells to serve as the white lines, fill with NAs
+  #   lines.width <- lines.width %||% ceiling(x = nrow(x = data.group) * 0.0025)
+  #   placeholder.cells <- sapply(
+  #     X = 1:(length(x = levels(x = group.use)) * lines.width),
+  #     FUN = function(x) {
+  #       return(RandomName(length = 20))
+  #     }
+  #   )
+  }
+
+
+group.use
+
+
+object <- suppressMessages(expr = StashIdent(object = seurat_Obj, save.name = 'ident'))
+
+data
 
 
 
 
 
+
+
+
+
+
+
+
+p <- DoHeatmap(seurat_Obj, features = features,slot = "data", ident = "cell.type.ident.by.data.set")
+p
+
+q <- ggplot(p$data, aes(Cell, Feature,fill= Expression, width = 1, height = 1)) + 
+  geom_tile() +
+  scale_fill_distiller(
+    palette = "RdYlBu") +
+  theme_ipsum()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=.5,size = 13),
+        axis.title.y.right = element_text(size=13),panel.spacing = unit(.35, "lines")) + facet_grid( ~ Identity, scales='free_x')
+
+cell.hm <- ggplot(p$data, aes(Cell, Feature,fill= Expression, width = 1, height = 1)) + 
+  geom_tile() +
+  scale_fill_distiller(
+    palette = "RdYlBu") +
+  theme_ipsum()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=.5,size = 13),
+        axis.title.y.right = element_text(size=13))
